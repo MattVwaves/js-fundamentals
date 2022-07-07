eb85436618304ebeb6baba2218af67b60dde40d4
MattVwaves <jamesmattholt@gmail.com>
2022-07-07 15:35:57 +0100

    cmpltd objt-kys

--- src/data-types/objects/object-keys.js
+++ src/data-types/objects/object-keys.js
@@ -2,13 +2,14 @@
 const book = {
   name: 'Clean Code',
   author: 'Robert C. Martin',
-  category: 'Cooking',
+  category: 'Programming',
   isbn: {
     isbn10: '9780132350884',
     asin: '0132350882'
   },
   publisher: 'Prentice Hall',
-  dimensions: '10x12x2'
+  dimensions: '10x12x2',
+  pages: 464,
 }
 
 const isbn13 = '978-0132350884'
@@ -16,11 +17,16 @@ const isbn13 = '978-0132350884'
 // TODO: write code in this section to pass the tests. You will need to add new code
 // as well as modify some of the existing code
 
+book.isbn.isbn13 = '978-0132350884'
+delete book.dimensions
+delete book.isbn.asin
+
+
 // Set this to the book name
-const name = ''
+const name = 'Clean Code'
 
 // Set this to the isbn 10 value
-const isbn10 = ''
+const isbn10 = '9780132350884'
 
 // Do not edit this exported object
 module.exports = {
